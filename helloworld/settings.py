e60961fe9d4ffb38bd7c52cac860b06d08b803b4
Hassan-YB <hassanbaras5@gmail.com>
2021-02-11 15:43:06 +0500

    heroku env

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -142,12 +142,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),]
+    os.path.join(BASE_DIR, 'sailor','static'),]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 
