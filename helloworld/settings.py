5c58d2526d4bbf940a4ebbac86249e6c2df9f629
Hassan-YB <hassanbaras5@gmail.com>
2021-02-11 13:29:04 +0500

    Latest Version

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o%i%*isj33dp$xp4hef(cra%_s0rq--#m%)qxi8g3acp)9-ntn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gbstates.herokuapp.com']
 
 
 # Application definition
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     #First 2 for sessions in allauth
     
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -150,3 +152,4 @@ MEDIA_URL = '/media/'
 #Allauth login redirect to homepage
 LOGIN_REDIRECT_URL = '/'
 
+django_heroku.settings(locals())
